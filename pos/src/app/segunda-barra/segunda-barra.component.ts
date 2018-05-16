5c3e2c580bc9ab0e16150c8737014f98570b021e
Jisargu <jonathnsaa@gmail.com>
2018-05-16 12:33:08 -0500

    Inicializacion del componente segunda barra de navegacion

---
+++ pos/src/app/segunda-barra/segunda-barra.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-segunda-barra',
+  templateUrl: './segunda-barra.component.html',
+  styleUrls: ['./segunda-barra.component.css']
+})
+export class SegundaBarraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
