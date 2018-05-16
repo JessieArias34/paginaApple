78da973d19c5a821dfa11a8ef047531c11ca2395
Jessica Arias <jessie.arias7@gmail.com>
2018-05-16 15:02:31 -0500

    principal

--- pos/src/app/app.module.ts
+++ pos/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppleComponent } from './apple/apple.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppleComponent
   ],
   imports: [
     BrowserModule
