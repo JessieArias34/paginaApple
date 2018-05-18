3681040bd142c18d651416286ff558b8ec22c4a3
Jisargu <jonathnsaa@gmail.com>
2018-05-17 22:23:02 -0500

    creacion de carpeta para clases y beautify code

--- pos/src/app/segunda-barra/segunda-barra.component.ts
+++ pos/src/app/segunda-barra/segunda-barra.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Barra2_ipad } from "../barra2_ipad";
-declare var $:any;
+import { Barra2_ipad } from "../../assets/class/barra2_ipad";
+declare var $: any;
 @Component({
   selector: 'app-segunda-barra',
   templateUrl: './segunda-barra.component.html',
@@ -8,34 +8,34 @@ declare var $:any;
 })
 
 export class SegundaBarraComponent implements OnInit {
-nuevo="Nuevo";
+  nuevo = "Nuevo";
 
-barra_ipad: Barra2_ipad={
-item1:"iPad Pro",
-item2:"iPad",
-item3:"iPad mini 4",
-item4:"iOS 11",
-item5:"Accesorios",
-item6:"Comparar",
- }
+  barra_ipad: Barra2_ipad = {
+    item1: "iPad Pro",
+    item2: "iPad",
+    item3: "iPad mini 4",
+    item4: "iOS 11",
+    item5: "Accesorios",
+    item6: "Comparar",
+  }
 
   constructor() { }
 
   ngOnInit() {
 
-    $('.boton_izq').click(function() {
+    $('.boton_izq').click(function () {
       event.preventDefault();
       $('#barra_ipad').animate({
         scrollLeft: "+=100px"
       }, "slow");
-   });
-   
-     $('.boton_der').click(function() {
+    });
+
+    $('.boton_der').click(function () {
       event.preventDefault();
       $('#barra_ipad').animate({
         scrollLeft: "-=100px"
       }, "slow");
-   });
+    });
   }
 
 }
