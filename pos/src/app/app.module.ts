7bb8c7cd2b13aac0092e9dd3d2870801ff95ca5d
Jisargu <jonathnsaa@gmail.com>
2018-05-18 00:42:55 -0500

    inicializando la imagen ultima de iphone

--- pos/src/app/app.module.ts
+++ pos/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SegundaBarraComponent } from './segunda-barra/segunda-barra.component';
 import { SegundaBarra2Component } from './segunda-barra2/segunda-barra2.component';
+import { UltimaiPhoneComponent } from './ultimai-phone/ultimai-phone.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SegundaBarraComponent,
-    SegundaBarra2Component
+    SegundaBarra2Component,
+    UltimaiPhoneComponent
   ],
   imports: [
     BrowserModule
---
+++ pos/src/app/ultimai-phone/ultimai-phone.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ultimai-phone',
+  templateUrl: './ultimai-phone.component.html',
+  styleUrls: ['./ultimai-phone.component.css']
+})
+export class UltimaiPhoneComponent implements OnInit {
+  titulo_imagen_iphone_last="El iPhone en la empresa";
+  texto_imagen_iphone_last_1="Más información sobre el iPhone en la empresa";
+  texto_imagen_iphone_last_2="Comprar para la empresa";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
